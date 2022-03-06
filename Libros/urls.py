9fe1164585e8d05d16f2b1415e7b3806f47c0455
carlos2172167 <ramirezcar710@gmail.com>
2022-03-06 11:53:02 -0500

    index html

--- Libros/urls.py
+++ Libros/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 
 
@@ -5,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
-    path('autores/', views.listarAutores, name='index' ),
+    path('autores/', views.listarAutores, name='autores' ),
+    path('autores/new', views.create_autor, name='new_autor'),
     path('libros/', views.listarBooks, name='index' )
 ]
--- Libros/views.py
+++ Libros/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic.detail import DetailView
 
 from .models import Autor, Book
+from .forms import AuthorForm
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    template = loader.get_template('libros/index.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
 
 def listarAutores(request):
     lista = Autor.objects.all()
     output = ', '.join([a.nombres for a in lista])
     return HttpResponse(output) 
+
+def create_autor(request):
+
+    context = {}
+
+    form = AuthorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('autores')
+    
+    context['form'] = form
+    return render(request,'autores/create_autor.html', context)
 
 def listarBooks(request):
     lista = Book.objects.all()
