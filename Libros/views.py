5c93d6219b637407793efe584b145893eb8359ac
carlos2172167 <ramirezcar710@gmail.com>
2022-03-05 21:13:52 -0500

    arreglo index

--- Libros/views.py
+++ Libros/views.py
@@ -1,6 +1,6 @@
 from ast import Return
 from re import A
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Autor
 # Create your views here.
